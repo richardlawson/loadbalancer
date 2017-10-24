8eb498a482a2fb48d5e6b61e979ab77b8fd2395f
Richard Lawson <richard.lawson@ed.ac.uk>
2017-10-24 21:52:10 +0100

    changed db to was reds

--- config.php
+++ config.php
@@ -10,7 +10,8 @@ if($_SERVER['SERVER_NAME'] == 'localhost'){
 	}
 }else{
 	class Config{
-		const  DB_HOST = "172.30.0.252";
+		//const  DB_HOST = "172.30.0.252";
+		const  DB_HOST = "flipsharkdbinstance.chf3iff6id95.eu-west-1.rds.amazonaws.com";
 		const  DB_USERNAME = "root";
 		const  DB_PASSWORD = "aberdeen";
 		const  DB_NAME = "test";
